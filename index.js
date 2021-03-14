4a6f29ecf749cf759e1b9b1043dce5389663e6db
joonwon <>
2021-03-14 15:27:43 +0900

    #2.7 DOM practice

--- index.js
+++ index.js
@@ -1,36 +1,60 @@
-// alert("Im Working. Im JS. Im Beautiful. Im worth it");
-// console.log("Im Working. Im JS. Im Beautiful. Im worth it");
+/** 실전 */
 
-const title = document.querySelector("#title");
+// const title = document.querySelector("#title");
+const title = document.querySelector(".btn");
 
-const BASE_COLOR = "rgb(255, 255, 255)";
-const RED_COLOR = "rgb(0, 255, 0)";
+const CLICKED_CLASS = "clicked";
 
-function handleOnClick(event) {
-  // console.log(title.style.color, BASE_COLOR);
-
-  if (title.style.color === BASE_COLOR) {
-    title.style.color = RED_COLOR;
-  } else {
-    title.style.color = BASE_COLOR;
-  }
+function handleOnClick() {
+  // const isClicked = title.classList.contains(CLICKED_CLASS);
+  // if (!isClicked) {
+  //   title.classList.add(CLICKED_CLASS);
+  // } else {
+  //   title.classList.remove(CLICKED_CLASS);
+  // }
+  title.classList.toggle(CLICKED_CLASS);
 }
 
 function init() {
-  title.style.color = BASE_COLOR;
-  // title.addEventListener("click", handleOnClick);
-  title.addEventListener("mouseenter", handleOnClick);
+  title.addEventListener("click", handleOnClick);
 }
 init();
 
-//test : 20210314 safari 에서는 동작 안함. chrome에서 동작 확인.
-function handleOnOnline() {
-  console.log("Hello!!");
-}
+/** control html and css */
+// const title = document.querySelector("#title");
 
-function handleOnOffline() {
-  console.log("ByeBye!!");
-}
+// const BASE_COLOR = "rgb(255, 255, 255)";
+// const RED_COLOR = "rgb(0, 255, 0)";
 
-window.addEventListener("online", handleOnOnline);
-window.addEventListener("offline", handleOnOffline);
+// function handleOnClick(event) {
+//   // console.log(title.style.color, BASE_COLOR);
+
+//   if (title.style.color === BASE_COLOR) {
+//     title.style.color = RED_COLOR;
+//   } else {
+//     title.style.color = BASE_COLOR;
+//   }
+// }
+
+// function init() {
+//   title.style.color = BASE_COLOR;
+//   // title.addEventListener("click", handleOnClick);
+//   title.addEventListener("mouseenter", handleOnClick);
+// }
+// init();
+
+// //test : 20210314 safari 에서는 동작 안함. chrome에서 동작 확인.
+// function handleOnOnline() {
+//   console.log("Hello!!");
+// }
+
+// function handleOnOffline() {
+//   console.log("ByeBye!!");
+// }
+
+// window.addEventListener("online", handleOnOnline);
+// window.addEventListener("offline", handleOnOffline);
+
+/** helloworld */
+// alert("Im Working. Im JS. Im Beautiful. Im worth it");
+// console.log("Im Working. Im JS. Im Beautiful. Im worth it");
